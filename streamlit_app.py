ca501b6045c9d0125ec17adee045c4b0d2668813
mehdihafezi <147152008+mehdihafezi@users.noreply.github.com>
2023-10-06 16:29:30 +0200

    Update streamlit_app.py
    typo fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,18 @@
 import streamlit as st
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
-st.title('My Parents New Healthy Diner')
-st.header('My Breakfast menu')
+
+
+#st.title('My Parents New Healthy Diner')
+#st.header('My Breakfast menu')
 #st.divider()  # 
-st.text('Omega 3 & Blueberry Oatmeal')
+#st.text('Omega 3 & Blueberry Oatmeal')
 #st.divider()  # 
-st.text('Kale, Spinach & Rocket Smoothie')
+#st.text('Kale, Spinach & Rocket Smoothie')
 #st.divider()  # 
-st.text('Hard-Boiled free-Range Egge')
+#st.text('Hard-Boiled free-Range Egge')
 #st.divider()  # 
 #st.write('Omega 3 & Blueberry Oatmeal')
