caf3b91378d65be99faaba2c1a2b17bf1867df3a
mehdihafezi <147152008+mehdihafezi@users.noreply.github.com>
2023-10-06 15:08:50 +0200

    Update streamlit_app.py
    add comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 st.title('My Parents New Healthy Diner')
 st.header('My Breakfast menu')
-st.divider()  # 
-st.write('Omega 3 & Blueberry Oatmeal')
-st.divider()  # 
-st.write('Kale, Spinach & Rocket Smoothie')
-st.divider()  # 
+#st.divider()  # 
+st.text('Omega 3 & Blueberry Oatmeal')
+#st.divider()  # 
+st.text('Kale, Spinach & Rocket Smoothie')
+#st.divider()  # 
 st.text('Hard-Boiled free-Range Egge')
-st.divider()  # 
+#st.divider()  # 
+#st.write('Omega 3 & Blueberry Oatmeal')
