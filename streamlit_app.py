4ccfed4c7d8a540ced4d7052f64f8ae06b972b21
mehdihafezi <147152008+mehdihafezi@users.noreply.github.com>
2023-10-06 14:43:10 +0200

    Update streamlit_app.py
    fixed breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text('Omega 3 & Blueberry Oatmeal Kale, Spinach & Rocket Smoothie Hard-Boiled free-Range Egge')
+streamlit.text(' My Breakfast menu : Omega 3 & Blueberry Oatmeal Kale, Spinach & Rocket Smoothie Hard-Boiled free-Range Egge')
 
