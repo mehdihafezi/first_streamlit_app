d0bea961823134db2e7ffd24978cbfaa19730add
mehdihafezi <147152008+mehdihafezi@users.noreply.github.com>
2023-10-06 16:35:14 +0200

    Update streamlit_app.py
    added Smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
 
 
 #st.title('My Parents New Healthy Diner')
